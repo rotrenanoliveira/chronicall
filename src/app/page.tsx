f94df09ae07306e59288992187ae633dbe55d54b
Renan Oliveira <rotrenanoliveira@gmail.com>
2024-11-01 16:33:00 -0300

    fix: text size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 h-screen w-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Bem-vindo ao chronicall</h1>
+      <h1 className="text-xl lg:text-4xl font-bold">Bem-vindo ao chronicall</h1>
       <Button variant="outline" asChild>
         <Link href="/sign-up">
           Entrar <LogIn className="size-4" />
